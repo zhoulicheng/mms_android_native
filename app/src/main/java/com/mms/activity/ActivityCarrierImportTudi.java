b7b3076492c07e219dac3049a20dc50901a37622
1215 <1465465@qq.com>
2016-05-03 16:29:11 +0800

    5.3.3

--- app/src/main/java/com/mms/activity/ActivityCarrierImportTudi.java
+++ app/src/main/java/com/mms/activity/ActivityCarrierImportTudi.java
@@ -6,12 +6,15 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.mms.R;
 import com.mms.base.BaseSwipeActivity;
+import com.mms.util.Utils;
 import com.mms.widget.ContactView;
 
 import java.util.ArrayList;
@@ -25,7 +28,7 @@ import roboguice.inject.InjectView;
  */
 
 @ContentView(R.layout.layout_activity_carrier_import_tudi)
-public class ActivityCarrierImportTudi extends BaseSwipeActivity implements View.OnClickListener {
+public class ActivityCarrierImportTudi extends BaseSwipeActivity implements View.OnClickListener,CompoundButton.OnCheckedChangeListener {
 
     @InjectView(R.id.et_activity_carrier_import_tudi_title)
     private EditText etTitle;
@@ -51,7 +54,20 @@ public class ActivityCarrierImportTudi extends BaseSwipeActivity implements View
     @InjectView(R.id.btn_activity_carrier_import_tudi_add_contact)
     private Button btnAddContact;
 
-    private Button btnDelContact;
+    @InjectView(R.id.cb_activity_carrier_import_tudi_zulin)
+    private CheckBox cbZulin;
+
+    @InjectView(R.id.cb_activity_carrier_import_tudi_gouzhi)
+    private CheckBox cbGouzhi;
+
+    @InjectView(R.id.cb_activity_carrier_import_tudi_hezuo)
+    private CheckBox cbHezuo;
+
+    @InjectView(R.id.cb_activity_carrier_import_tudi_guapai)
+    private CheckBox cbGuapai;
+
+    @InjectView(R.id.cb_activity_carrier_import_tudi_zhuanrang)
+    private CheckBox cbZhuanrang;
 
     private boolean hasBtnDelContact = false;
 
@@ -79,6 +95,11 @@ public class ActivityCarrierImportTudi extends BaseSwipeActivity implements View
         btnAddressDel.setOnClickListener(this);
         rlLevel.setOnClickListener(this);
         btnAddContact.setOnClickListener(this);
+        cbGouzhi.setOnCheckedChangeListener(this);
+        cbGuapai.setOnCheckedChangeListener(this);
+        cbHezuo.setOnCheckedChangeListener(this);
+        cbZhuanrang.setOnCheckedChangeListener(this);
+        cbZulin.setOnCheckedChangeListener(this);
 
     }
 
@@ -137,47 +158,71 @@ public class ActivityCarrierImportTudi extends BaseSwipeActivity implements View
                 break;
             case R.id.btn_activity_carrier_import_tudi_add_contact:
                 //增加一个联系人
-                ContactView contactView = new ContactView(this);
-                contactViews.add(contactView);
-                llContacts.addView(contactView, LP_FW);
-                if (contactViews.size() > 1 && !hasBtnDelContact) {
-                    addBtnDelContact();
-                }
+                addContact();
                 break;
             case R.id.btn_activity_carrier_import_tudi_del_contact:
-                //删除最后一个联系人
-                if (contactViews.size() > 1) {
-                    llContacts.removeView(contactViews.get(contactViews.size() - 1));
-                    contactViews.remove(contactViews.get(contactViews.size() - 1));
-                    if (contactViews.size() == 1) {
-                        llAddAndDel.removeView(view);
-                        hasBtnDelContact = false;
-                    }
-                }
+                //删除联系人
+                delContact(view);
                 break;
 
         }
     }
 
-    private void addBtnDelContact(){
-        btnDelContact = new Button(this);
-        LinearLayout.LayoutParams LP_BTN = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT);
-        LP_BTN.weight = 1;
-        if (Build.VERSION.SDK_INT >= 16 && Build.VERSION.SDK_INT < 21) {
-            btnDelContact.setBackground(getResources().getDrawable(R.drawable.whitebuttonselector));
-        } else if (Build.VERSION.SDK_INT >= 21) {
-            btnDelContact.setBackground(getResources().getDrawable(R.drawable.whitebuttonselector, null));
-        } else {
-            btnDelContact.setBackgroundDrawable(getResources().getDrawable(R.drawable.whitebuttonselector));
+    private void addContact() {
+        ContactView contactView = new ContactView(this);
+        contactViews.add(contactView);
+        llContacts.addView(contactView, LP_FW);
+        if (contactViews.size() > 1 && !hasBtnDelContact) {
+            Button btnDelContact = new Button(this);
+            LinearLayout.LayoutParams LP_BTN = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT);
+            LP_BTN.weight = 1;
+            if (Build.VERSION.SDK_INT >= 16 && Build.VERSION.SDK_INT < 21) {
+                btnDelContact.setBackground(getResources().getDrawable(R.drawable.whitebuttonselector));
+            } else if (Build.VERSION.SDK_INT >= 21) {
+                btnDelContact.setBackground(getResources().getDrawable(R.drawable.whitebuttonselector, null));
+            } else {
+                btnDelContact.setBackgroundDrawable(getResources().getDrawable(R.drawable.whitebuttonselector));
+            }
+            btnDelContact.setId(R.id.btn_activity_carrier_import_tudi_del_contact);
+            btnDelContact.setText("删除联系人");
+            btnDelContact.setTextSize(17);
+            btnDelContact.setTextColor(getResources().getColor(R.color.myRed));
+            btnDelContact.setOnClickListener(this);
+            llAddAndDel.addView(btnDelContact, LP_BTN);
+            hasBtnDelContact = true;
         }
-        btnDelContact.setId(R.id.btn_activity_carrier_import_tudi_del_contact);
-        btnDelContact.setText("删除联系人");
-        btnDelContact.setTextSize(17);
-        btnDelContact.setTextColor(getResources().getColor(R.color.myRed));
-        btnDelContact.setOnClickListener(this);
-        llAddAndDel.addView(btnDelContact, LP_BTN);
-        hasBtnDelContact = true;
+    }
+
+    private void delContact(View view) {
+        if (contactViews.size() > 1) {
+            llContacts.removeView(contactViews.get(contactViews.size() - 1));
+            contactViews.remove(contactViews.get(contactViews.size() - 1));
+            if (contactViews.size() == 1) {
+                llAddAndDel.removeView(view);
+                hasBtnDelContact = false;
+            }
+        }
     }
 
 
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        switch (compoundButton.getId()){
+            case R.id.cb_activity_carrier_import_tudi_zulin:
+                if (b){
+                    Utils.showToast(this,"您选择了租赁");
+                }else {
+                    Utils.showToast(this,"您取消了租赁");
+                }
+                break;
+            case R.id.cb_activity_carrier_import_tudi_gouzhi:
+                break;
+            case R.id.cb_activity_carrier_import_tudi_hezuo:
+                break;
+            case R.id.cb_activity_carrier_import_tudi_guapai:
+                break;
+            case R.id.cb_activity_carrier_import_tudi_zhuanrang:
+                break;
+        }
+    }
 }
